59317bfab31f5c3e56b2693497af4aa0050d6912
Vladimir Kabysh <v.kabish@ukr.net>
2018-09-08 13:46:03 +0300

    fix undefined onload

--- js-basics/lesson7/src/index.js
+++ js-basics/lesson7/src/index.js
@@ -1,16 +1,11 @@
 import { renderUserData } from './controller';
 
-const startRender = () => {  
-  const rootEl = document.getElementById('root');
-  renderUserData(rootEl);
-};
-
 window.addEventListener('load', () => {
   window.location = '#/';
-  startRender(); 
 });
 
 window.addEventListener('hashchange', () => {
-  startRender(); 
+  const rootEl = document.getElementById('root');
+  renderUserData(rootEl);
 });
 
