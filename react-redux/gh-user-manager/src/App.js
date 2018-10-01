ac493ffd73694e083fd49d264f16da2da360ea74
Vladimir Kabysh <v.kabish@ukr.net>
2018-10-01 20:03:04 +0300

    add gh user manager app

--- react-redux/gh-user-manager/src/App.js
+++ react-redux/gh-user-manager/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import UserView from './components/UserProfileManager/UserView';
 import UserEditView from './components/UserProfileManager/UserEditView';
 
